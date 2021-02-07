353722230dfe047215483073531be8c50a020521
Darakah <darakah.dark@gmail.com>
2021-02-07 05:21:29 +0100

    Version 0.1.2

--- main.ts
+++ main.ts
@@ -1,26 +1,32 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, FrontMatterCache, CachedMetadata } from 'obsidian';
 
-interface MyPluginSettings {
+interface PapercutSettings {
 	PAPER_CUT_TAG: string;
 	PAPER_CUT_LIMIT: number;
 	APPLY_TO_ALL: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: PapercutSettings = {
 	PAPER_CUT_TAG: 'papercut',
 	PAPER_CUT_LIMIT: 5000,
 	APPLY_TO_ALL: false
 }
 
-// Set node attribute to value
-function attr(node: any, attribute: string, value: string) {
-    if (value == null)
-        node.removeAttribute(attribute);
-    else if (node.getAttribute(attribute) !== value)
-        node.setAttribute(attribute, value);}
+export default class PapercutPlugin extends Plugin {
+	settings: PapercutSettings;
+    fill_perc: number;
+    page_len: number;
+    page_cache: CachedMetadata;
+    page_content: string;
+    statusBar: HTMLElement;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    super(){
+        fill_perc: this.fill_perc;
+        page_len: this.page_len;
+        page_cache: this.page_cache;
+        page_content: this.page_content;
+        statusBar: this.statusBar;
+    }
 
 	async onload() {
         // Send output to console
@@ -35,10 +41,8 @@ export default class MyPlugin extends Plugin {
         this.registerEvent(this.app.vault.on("delete", this.cut));
 
         // Add status bar to update
-        var status = this.addStatusBarItem();
-        attr(status, 'class', 'papercut')
-
-       	this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.statusBar = this.addStatusBarItem();
+       	this.addSettingTab(new PapercutSettingTab(this.app, this));
 
         // Run cut function on load
         this.cut();
@@ -46,57 +50,54 @@ export default class MyPlugin extends Plugin {
 
 	cut(){
 		// Only work if the current Active file is an md note
-         if(this.app.workspace.getActiveFile() != undefined) {
+         if(this.app.workspace.getActiveFile()) {
             // Only proceed if current page has a paper cut tab
-            var frontmatter = this.app.metadataCache.getFileCache(this.app.workspace.getActiveFile()).frontmatter;
-            var content = (this.app.workspace.getActiveFile() as any).cachedData;
-            var len = content.replace(/[^a-zA-Z0-9]/g, '').length;
+            this.page_cache = this.app.metadataCache.getFileCache(this.app.workspace.getActiveFile());
 
-            if(this.settings.APPLY_TO_ALL || (frontmatter != undefined)){
-                if(this.settings.APPLY_TO_ALL || frontmatter.tags.includes(this.settings.PAPER_CUT_TAG)){
+            if(this.settings.APPLY_TO_ALL || this.page_cache){
+                if(this.settings.APPLY_TO_ALL || this.page_cache.frontmatter.tags.includes(this.settings.PAPER_CUT_TAG)){
+                 this.app.vault.adapter.read(this.app.workspace.getActiveFile().path).then((value) => { this.page_content = value;
+                 this.page_len = this.page_content.replace(/[^a-zA-Z0-9]/g, '').length;});
                 // Check the number of characters on the current active page
-                var perc = (len / this.settings.PAPER_CUT_LIMIT) * 100
+                this.fill_perc = (this.page_len / this.settings.PAPER_CUT_LIMIT) * 100
 
-                let i, mid, lhs, rhs, tmp;
-                var tmp_content, tmp_counter;
+                let mid, lhs, rhs, tmp_content;
                 tmp_content = "";
-                tmp_counter = this.settings.PAPER_CUT_LIMIT;
 
-                if(len > this.settings.PAPER_CUT_LIMIT){
-                    (this.app as any).statusBar.containerEl.querySelector("div[class='papercut']").innerHTML = '<span style="color:red;">100 %</span>';
+                if(this.page_len > this.settings.PAPER_CUT_LIMIT){
+                    this.statusBar.setText('100 %');
                     // get the illegal text
                     lhs = this.settings.PAPER_CUT_LIMIT;
-                    rhs = content.length;
+                    rhs = this.page_content.length;
 
                     while ((tmp_content.replace(/[^a-zA-Z0-9]/g, '').length < this.settings.PAPER_CUT_LIMIT) && (lhs < rhs)) {
                         mid = lhs + (rhs - lhs) / 2;
-                        tmp = tmp_content + content.substring(tmp_content.length, mid);
+                        let tmp = tmp_content + this.page_content.substring(tmp_content.length, mid);
                         if (tmp.replace(/[^a-zA-Z0-9]/g, '').length < this.settings.PAPER_CUT_LIMIT) {
                             lhs = mid + 1;
-                            tmp_content = tmp_content + content.substring(tmp_content.length, mid);
+                            tmp_content = tmp_content + this.page_content.substring(tmp_content.length, mid);
                         }
                         else {
                             rhs = mid - 1;
                         }
                     }
 
-                    this.app.vault.adapter.write(this.app.workspace.getActiveFile().path, content.substring(0, mid))
-                    //console.log(content.substring(0, tmp_counter))
+                    this.app.vault.adapter.write(this.app.workspace.getActiveFile().path, this.page_content.substring(0, mid))
 
                 } else {
-                    (this.app as any).statusBar.containerEl.querySelector("div[class='papercut']").innerHTML = Math.round(perc * 100) / 100 + " %"
+                    this.statusBar.setText(Math.round(this.fill_perc * 100) / 100 + " %")
                     //console.log('limit not reached');
                 }
             } else {
-                (this.app as any).statusBar.containerEl.querySelector("div[class='papercut']").innerHTML = '';
+                this.statusBar.setText('');
                 //console.log('Not a tagged file!');
             }
             } else {
-                (this.app as any).statusBar.containerEl.querySelector("div[class='papercut']").innerHTML = '';
+                this.statusBar.setText('');
                 //console.log('No frontmatter!');
             }
             }else {
-                (this.app as any).statusBar.containerEl.querySelector("div[class='papercut']").innerHTML = '';
+                this.statusBar.setText('');
                 //console.log('Not a note active file!')
             }
 		return;
@@ -115,10 +116,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class PapercutSettingTab extends PluginSettingTab {
+	plugin: PapercutPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PapercutPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -138,6 +139,7 @@ class SampleSettingTab extends PluginSettingTab {
 				.onChange(async (value) => {
 					console.log('Secret: ' + value);
 					this.plugin.settings.PAPER_CUT_TAG = value;
+                    await this.plugin.saveSettings();
 				}));
 
 		new Setting(containerEl)
@@ -149,6 +151,7 @@ class SampleSettingTab extends PluginSettingTab {
 				.onChange(async (value) => {
 					console.log('Secret: ' + value);
 					this.plugin.settings.PAPER_CUT_LIMIT = +value;
+                    await this.plugin.saveSettings();
 				}));
 
 		new Setting(containerEl)
