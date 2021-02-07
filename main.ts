a8d001fbb47bdd0d6a44857596a50b858dac759d
Darakah <darakah.dark@gmail.com>
2021-02-07 13:50:18 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -14,24 +14,15 @@ const DEFAULT_SETTINGS: PapercutSettings = {
 
 export default class PapercutPlugin extends Plugin {
 	settings: PapercutSettings;
-    fill_perc: number;
-    page_len: number;
-    page_cache: CachedMetadata;
-    page_content: string;
     statusBar: HTMLElement;
 
     super(){
-        fill_perc: this.fill_perc;
-        page_len: this.page_len;
-        page_cache: this.page_cache;
-        page_content: this.page_content;
         statusBar: this.statusBar;
     }
 
 	async onload() {
         // Send output to console
-        console.log('Loaded Plugin');
-
+        console.log('Loaded Comments Plugin');
         await this.loadSettings();
 
         this.cut = this.cut.bind(this);
@@ -48,59 +39,77 @@ export default class PapercutPlugin extends Plugin {
         this.cut();
 	}
 
-	cut(){
+	async cut(){
 		// Only work if the current Active file is an md note
-         if(this.app.workspace.getActiveFile()) {
-            // Only proceed if current page has a paper cut tab
-            this.page_cache = this.app.metadataCache.getFileCache(this.app.workspace.getActiveFile());
+        let active_leaf = this.app.workspace.getActiveFile()
+        // if its not an active leaf remove status bar and return
+        if(!active_leaf){
+            this.statusBar.empty();
+            return;
+        } 
+        
+        // check if active leaf has metadataCache
+        let page_cache = this.app.metadataCache.getFileCache(active_leaf);
+        if(!page_cache){
+            this.statusBar.empty();
+            return;
+        }
 
-            if(this.settings.APPLY_TO_ALL || this.page_cache){
-                if(this.settings.APPLY_TO_ALL || this.page_cache.frontmatter.tags.includes(this.settings.PAPER_CUT_TAG)){
-                 this.app.vault.adapter.read(this.app.workspace.getActiveFile().path).then((value) => { this.page_content = value;
-                 this.page_len = this.page_content.replace(/[^a-zA-Z0-9]/g, '').length;});
-                // Check the number of characters on the current active page
-                this.fill_perc = (this.page_len / this.settings.PAPER_CUT_LIMIT) * 100
+        // if the apply to all condition is not active
+        if(!this.settings.APPLY_TO_ALL){
+            // check for existance of frontmatter
+            if(!page_cache.frontmatter){
+                this.statusBar.empty();
+                return;  
+            }
+            
+            // check if the frontmatter has tags info
+            if(!page_cache.frontmatter.tags){
+                this.statusBar.empty();
+                return;     
+            }
 
-                let mid, lhs, rhs, tmp_content;
-                tmp_content = "";
+            // check if the plugin tag is included in the list
+            if(!page_cache.frontmatter.tags.includes(this.settings.PAPER_CUT_TAG)){
+                this.statusBar.empty();
+                return;     
+            }           
+        }
 
-                if(this.page_len > this.settings.PAPER_CUT_LIMIT){
-                    this.statusBar.setText('100 %');
-                    // get the illegal text
-                    lhs = this.settings.PAPER_CUT_LIMIT;
-                    rhs = this.page_content.length;
-
-                    while ((tmp_content.replace(/[^a-zA-Z0-9]/g, '').length < this.settings.PAPER_CUT_LIMIT) && (lhs < rhs)) {
-                        mid = lhs + (rhs - lhs) / 2;
-                        let tmp = tmp_content + this.page_content.substring(tmp_content.length, mid);
-                        if (tmp.replace(/[^a-zA-Z0-9]/g, '').length < this.settings.PAPER_CUT_LIMIT) {
-                            lhs = mid + 1;
-                            tmp_content = tmp_content + this.page_content.substring(tmp_content.length, mid);
-                        }
-                        else {
-                            rhs = mid - 1;
-                        }
-                    }
-
-                    this.app.vault.adapter.write(this.app.workspace.getActiveFile().path, this.page_content.substring(0, mid))
-
-                } else {
-                    this.statusBar.setText(Math.round(this.fill_perc * 100) / 100 + " %")
-                    //console.log('limit not reached');
+        // All checks passed, active leaf has cache & either the apply to all option
+        // is chosen or the page has the correct tab
+        let page_content = await this.app.vault.read(active_leaf); 
+        // Check the number of characters on the current active page
+        let page_len = page_content.replace(/[\s\*=-]/g, '').length;
+        let fill_perc = (page_len / this.settings.PAPER_CUT_LIMIT) * 100;
+        
+        let mid, lhs, rhs, tmp_content;
+        tmp_content = "";
+        
+        if(page_len > this.settings.PAPER_CUT_LIMIT){
+            this.statusBar.setText('100 %');
+            // get the illegal text
+            lhs = this.settings.PAPER_CUT_LIMIT;
+            rhs = page_content.length;
+        
+            while ((tmp_content.replace(/[\s\*=-]/g, '').length < this.settings.PAPER_CUT_LIMIT) && (lhs < rhs)) {
+                mid = lhs + (rhs - lhs) / 2;
+                let tmp = tmp_content + page_content.substring(tmp_content.length, mid);
+                if (tmp.replace(/[\s\*=-]/g, '').length < this.settings.PAPER_CUT_LIMIT) {
+                    lhs = mid + 1;
+                    tmp_content = tmp_content + page_content.substring(tmp_content.length, mid);
                 }
-            } else {
-                this.statusBar.setText('');
-                //console.log('Not a tagged file!');
+                else {
+                    rhs = mid - 1;
+                }
             }
-            } else {
-                this.statusBar.setText('');
-                //console.log('No frontmatter!');
-            }
-            }else {
-                this.statusBar.setText('');
-                //console.log('Not a note active file!')
-            }
-		return;
+            // update the page content
+            this.app.vault.adapter.write(active_leaf.path, page_content.substring(0, mid));
+            return;
+        } else {
+            this.statusBar.setText(Math.floor(fill_perc * 100) / 100 + " %");
+            return;
+        }
 	}
 
 	onunload() {
@@ -137,7 +146,6 @@ class PapercutSettingTab extends PluginSettingTab {
 				.setPlaceholder(this.plugin.settings.PAPER_CUT_TAG)
 				.setValue('')
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
 					this.plugin.settings.PAPER_CUT_TAG = value;
                     await this.plugin.saveSettings();
 				}));
@@ -149,7 +157,6 @@ class PapercutSettingTab extends PluginSettingTab {
 				.setPlaceholder("" + this.plugin.settings.PAPER_CUT_LIMIT)
 				.setValue('')
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
 					this.plugin.settings.PAPER_CUT_LIMIT = +value;
                     await this.plugin.saveSettings();
 				}));
